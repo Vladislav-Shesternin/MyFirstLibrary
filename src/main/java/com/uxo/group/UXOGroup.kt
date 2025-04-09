239386876068609e68722cc39afb3f4a6dea5b79
Vladislav Shesternin <54269161+Vladislav-Shesternin@users.noreply.github.com>
2025-04-09 16:17:59 +0200

    v3

--- src/main/java/com/uxo/group/UXOGroup.kt
+++ src/main/java/com/uxo/group/UXOGroup.kt
@@ -1,35 +1,17 @@
 package com.uxo.group
 
-import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.scenes.scene2d.Group
-import com.badlogic.gdx.graphics.g2d.Batch
 
 class UXOGroup : Group() {
 
-//    fun initialize() {
-//        val vertexShaderStream = javaClass.classLoader.getResourceAsStream("shader/defaultVS.glsl")
-//        val fragmentShaderStream = javaClass.classLoader.getResourceAsStream("shader/maskFS.glsl")
-//    }
-
-    fun a1(): String {
-        val s = object {}.javaClass.getResource("shader/defaultVS.glsl")?.readText() ?: throw IllegalStateException("Shader not found")
-        return s
-    }
-
-    fun a2(): String {
-        val s = javaClass.getResource("shader/defaultVS.glsl")?.readText() ?: throw IllegalStateException("Shader not found")
-        return s
-    }
-
-    fun a3(): String {
-        val s = object {}.javaClass.getResource("/shader/defaultVS.glsl")?.readText() ?: throw IllegalStateException("Shader not found")
-        return s
-    }
-
-    fun a4(): String {
+    fun internalFun(): String {
         val s = javaClass.getResource("/shader/defaultVS.glsl")?.readText() ?: throw IllegalStateException("Shader not found")
         return s
     }
+    
+}
 
-
+fun globalFun(): String {
+    val s = object {}.javaClass.getResource("/shader/defaultVS.glsl")?.readText() ?: throw IllegalStateException("Shader not found")
+    return s
 }
